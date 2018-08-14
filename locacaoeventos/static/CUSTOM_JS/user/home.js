3a2ce912edc143e03c24a11ec33b87a5920364e4
Chris <christian.hukai@gmail.com>
2018-08-14 13:59:06 -0300

    home page

--- locacaoeventos/static/CUSTOM_JS/user/home.js
+++ locacaoeventos/static/CUSTOM_JS/user/home.js
@@ -1,3 +1,6 @@
 $('#date_id').datepicker({ dateFormat: 'dd/mm/yy' }).val()
 $('#date_id').mask("00/00/0000",{placeholder:"Data"});
 
+$("#home_arrow").click(function(){
+	$("#home_advanced").slideToggle("slow")
+})
