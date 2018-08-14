7ca28bee418b3eab3b66e250a9708f8523a69f4f
Chris <christian.hukai@gmail.com>
2018-08-14 15:08:00 -0300

    home page

--- locacaoeventos/static/CUSTOM_JS/user/home.js
+++ locacaoeventos/static/CUSTOM_JS/user/home.js
@@ -1,11 +1,13 @@
-$('#date_id').datepicker({ dateFormat: 'dd/mm/yy' }).val()
-$('#date_id').mask("00/00/0000",{placeholder:"Data"});
+$(document).ready(function(){
+	$('#date_id').datepicker({ dateFormat: 'dd/mm/yy' }).val()
+	$('#date_id').mask("00/00/0000",{placeholder:"Data"});
 
-$("#home_arrow").click(function(){
-	if($("#home_arrow_symbol").html()=="˅") {
-		$("#home_arrow_symbol").html("&#708;")
-	} else {
-		$("#home_arrow_symbol").html("&#709;")
-	}
-	$("#home_advanced").slideToggle("slow")
+	$("#home_arrow").click(function(){
+		if($("#home_arrow_symbol").html()=="˅") {
+			$("#home_arrow_symbol").html("&#708;")
+		} else {
+			$("#home_arrow_symbol").html("&#709;")
+		}
+		$("#home_advanced").slideToggle("slow")
+	})	
 })
