24c42677e2d250e5f169d5cebb30f78cfd4ae438
Chris <christian.hukai@gmail.com>
2018-11-02 13:42:22 -0300

    featured, order by, search

--- locacaoeventos/apps/user/buyerprofile/models.py
+++ locacaoeventos/apps/user/buyerprofile/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from locacaoeventos.apps.place.placecore.models import Place
-from locacaoeventos.apps.place.placereservation.models import PlacePrice
+# from locacaoeventos.apps.place.placereservation.models import PlacePrice
 
 class BuyerProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Usuário Comprador+")
