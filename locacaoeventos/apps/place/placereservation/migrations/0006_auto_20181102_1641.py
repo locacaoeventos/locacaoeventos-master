289e1459193ec5f161ddff0605a4ac7763475203
Chris <christian.hukai@gmail.com>
2018-12-20 21:29:21 -0200

    sprint

--- locacaoeventos/apps/place/placereservation/migrations/0006_auto_20181102_1641.py
+++ locacaoeventos/apps/place/placereservation/migrations/0006_auto_20181102_1641.py
@@ -20,7 +20,5 @@ class Migration(migrations.Migration):
             name='placeprice',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='placecore.PlacePrice'),
         ),
-        migrations.DeleteModel(
-            name='PlacePrice',
-        ),
+
     ]
