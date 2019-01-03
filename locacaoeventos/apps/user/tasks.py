bae3557c3e12f343b2e38c73260e701625c0524f
Chris <christian.hukai@gmail.com>
2019-01-02 23:50:39 -0200

    celery2

--- locacaoeventos/apps/user/tasks.py
+++ locacaoeventos/apps/user/tasks.py
@@ -14,7 +14,7 @@ logger = get_task_logger(__name__)
 # A periodic task that will run every minute (the symbol "*" means every)
 @periodic_task(run_every=(crontab(hour="*", minute="*", day_of_week="*")))
 def scraper_send():
-    str_titulo = str(datetime.datetime.now())
+    str_titulo = str("teste")
     str_body = "Oi Alec!!"
     to_email = "alecsander343@gmail.com"
     send_mail(str_titulo, str_body, 'christian.org96@gmail.com', [to_email], fail_silently=False)
