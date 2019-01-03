6b19d9f4cdddd054d784a17ee3109534d17b6dd4
Chris <christian.hukai@gmail.com>
2019-01-02 23:40:56 -0200

    celery2

--- locacaoeventos/settings/development.py
+++ locacaoeventos/settings/development.py
@@ -14,5 +14,5 @@ DEBUG = False
 # }
 
 # BROKER_URL = 'amqp://guest:guest@localhost:22225//'
-BROKER_URL = "amqp://locacao123f:locacaoqwe@localhost:22349/locacao123f"
+BROKER_URL = "amqp://locacao123f:locacaoqwe@localhost:5672/locacao123f"
 # BROKER_URL = "amqp://locacao123f:locacaoqwe@localhost:18941/locacao123f"
